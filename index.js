80978fe5ae7c8221c2ae3d8a83c16f2491e7ab72
OrderIt <orderit.devs@gmail.com>
2025-08-07 23:24:56 +0530

    Update index.js gooo

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log("Hello, World!");
-console.log("This is a simple Node.js applicationdfadfsfsdfdgfhdfghddrsgsde.");
+console.log("This is a simple Node.js applicationdfadfsfsdfdgfhdfghddrsgsbhhjhjnde.");
